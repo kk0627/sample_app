ce0195bf8da85c57b69fb889bdb89d97c33b0f18
keitakutsuno <keita@dotinstall.com>
2015-11-13 01:49:27 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,6 +32,11 @@ module SessionsHelper
     end
   end
 
+    # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -43,4 +48,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+    # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
